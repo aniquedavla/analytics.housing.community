6e797eb4e2e266dc9ea0022e299d920df9384f20
Anique Davla <aniquedavla@gmail.com>
2020-04-24 15:02:38 -0700

    frontend added

---
+++ a.hc.frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title></Title>
+    </div>
+  );
+}
+
+export default App;
---
+++ a.hc.frontend/src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Title = () => (
+
+  <div>
+    <h2 className="titleColor" color="#3f51b5"> Analytics - housing.community </h2>
+  </div>
+
+);
+export default Title;
