fd6c741caf35d0919c03d8afc76b4657615abfa1
Anique Davla <aniquedavla@gmail.com>
2020-04-30 17:27:51 -0700

    chart with json

--- analytics.hc.frontend/src/Components/UsersChart.jsx
+++ analytics.hc.frontend/src/Components/UsersChart.jsx
@@ -3,56 +3,62 @@ import { useTheme } from '@material-ui/core/styles';
 import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 import Title from './Title';
 
-// Generate objects
-function createData(month, amount) {
-  return { month, amount };
-}
-
-const data = [
-  createData('Jan', 50),
-  createData('Feb', 300),
-  createData('Mar', 600),
-  createData('Apr', 800),
-  createData('May', 1500),
-  createData('Jun', 2000),
-  createData('Jul', 2590),
-  createData('Aug', 2900),
-  createData('Sep', 4020),
-  createData('Oct', 5500),
-  createData('Nov', 7500),
-  createData('Dec', 9500),
-];
-
-export default function Chart() {
-  const theme = useTheme();
-    console.log(data);
-  return (
-    <React.Fragment>
-      <Title>New users in 2021</Title>
-      <ResponsiveContainer>
-        <LineChart
-          data={data}
-          margin={{
-            top: 10,
-            right: 10,
-            bottom: 0,
-            left: 16,
-          }}
-        >
-          <XAxis dataKey="month" stroke={theme.palette.text.secondary}>
-          </XAxis>
-          <YAxis stroke={theme.palette.text.secondary}>
-            <Label
-              angle={270}
-              position="left"
-              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
+class UsersChart extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newUsersByMonth:[],
+      isLoaded:false
+    }
+  }
+  componentDidMount(){
+    fetch('http://my-json-server.typicode.com/aniquedavla/demo/newUsersByMonth')
+          .then(response => response.json())
+          .then(json => this.setState({
+              newUsersByMonth: json,
+              isLoaded: true
+          }));
+  }
+  render(){
+    console.log(this.state.newUsersByMonth);
+    if(this.state.isLoaded){
+      return (
+        <React.Fragment>
+          <Title>New users in 2021</Title>
+          <ResponsiveContainer>
+            <LineChart
+              data={this.state.newUsersByMonth}
+              margin={{
+                top: 10,
+                right: 10,
+                bottom: 0,
+                left: 16,
+              }}
             >
-            Users (thousands)
-            </Label>
-          </YAxis>
-          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={true} />
-        </LineChart>
-      </ResponsiveContainer>
-    </React.Fragment>
-  );
+              <XAxis dataKey="month" >
+              </XAxis>
+              <YAxis >
+                <Label
+                  angle={270}
+                  position="left"
+                  style={{ textAnchor: 'middle'}}
+                >
+                Users (thousands)
+                </Label>
+              </YAxis>
+              <Line type="monotone" dataKey="amount" dot={true} />
+            </LineChart>
+          </ResponsiveContainer>
+        </React.Fragment>
+      );
+    } else {
+        return(
+          <div>
+            <Title>New users in 2021</Title>
+            loading...
+          </div>
+        )
+    }
+  }
 }
+export default UsersChart
