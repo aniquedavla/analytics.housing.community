aa89fbd2ed1f829d067ce0667234080df9fed5dd
Anique Davla <aniquedavla@gmail.com>
2020-05-10 01:11:17 -0700

    build

---
+++ analytics.hc.frontend/build/precache-manifest.06c67a3e0c997478899efcb8d73927b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717cc80a1e06e06ae964e62bbc7ce456",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fa4e7d9e3ccff47c7aa",
+    "url": "/static/css/main.e5312019.chunk.css"
+  },
+  {
+    "revision": "77d7571d3e789d576229",
+    "url": "/static/js/2.3a94cd78.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.3a94cd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa4e7d9e3ccff47c7aa",
+    "url": "/static/js/main.90d05879.chunk.js"
+  },
+  {
+    "revision": "6174bcbeb4b9e6a92730",
+    "url": "/static/js/runtime-main.1d6441d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
